27b0dbb1f71ac3f9361494e9ae3de983ed6dd563
stephane.malho <stepahen.malho@gmail.com>
2022-10-26 18:39:22 +0200

    F07-C-DataTables

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Sort, Filter, Page, Selection, Inject, Edit, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid, contextMenuItems } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        toolbar={['Delete']}
+        width="auto"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={{ allowEditing: true, allowAdding: true, allowDeleting: true }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[ Page, Toolbar, Sort, Selection, Edit, Filter ]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Search, Page, Toolbar, Inject, Sort } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid, contextMenuItems } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div>Employees</div>
-  )
-}
-
-export default Employees
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        toolbar={['Search']}
+        width="auto"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search, Toolbar, Sort ]} />
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
